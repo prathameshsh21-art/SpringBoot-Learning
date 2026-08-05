2d32f0f591c34aaad8b2e853facdcb3846ef8f9b
Prathamesh <prathamesh.sh21@gmail.com>
2026-08-05 16:45:31 +0530

    Add BusinessCalculationService with max finder

---
+++ Day03-Dependency-Injection/BusinessCalculationService.java
@@ -0,0 +1,27 @@
+package PracticeExample;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BusinessCalculationService {
+
+    private DataService dataService;
+
+    public BusinessCalculationService(
+            @Qualifier("mysql") DataService dataService) {
+
+        this.dataService = dataService;
+    }
+
+    public int findMax() {
+
+        return Arrays.stream(dataService.retrieveData())
+                .max()
+                .orElse(0);
+
+    }
+
+}
