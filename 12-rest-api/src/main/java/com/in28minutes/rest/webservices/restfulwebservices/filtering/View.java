7c543fd410fe35f8598a9e62f7e2dbdb4d1241f9
Prathamesh Kumbar <prathamesh.sh21@gmail.com>
2026-08-08 22:39:36 +0530

    Initial Spring and spring Boot learning

---
+++ 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/View.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restfulwebservices.filtering;
+
+
+
+public class View {
+
+    public interface View1 {
+
+
+
+    }
+
+
+
+    public interface View2 {
+
+
+
+    }
+
+
+
+}
